342c76a389f4fe4b1372c55a9e766ce8c094f8af
GaryKo <sysugjf@gmail.com>
2015-11-17 17:34:11 -0500

    push test case into test.cc

--- test.cc
+++ test.cc
@@ -38,13 +38,15 @@ int main()
 	return 0;
 }
 
+// av acd  efd efdsa dfef efd se ef 
+
 // a v  4	 1 		 1
 // b c  3	 4       1
 // c d  2   15      1
 // d e  5   1   5   1
 
 // rob entries = 64
-// R1=12, R2=32, F20=3.0
+// R1=12, R2=32, F20=3.0, R3=12, R4=32
 // Mem[4]=3.0, Mem[8]=2.0, Mem[12]=1.0, Mem[24]=6.0, Mem[28]=5.0, Mem[32]=4.0
 
 // Ld F2, 0(R1)
@@ -52,16 +54,18 @@ int main()
 // Ld F6, 0(R2)
 // Add.d F6, F4, F6
 // Sd F6, 0(R2)
-// Addi R1, R1, 20
-// Add R2, R2, R2
+// Addi R3, R3, 5
+// Addi R4, R4, -5
+// Bne R3, R4, -8
 // Add.d F20, F2, F2
 
-// Ld F2, 0(R1)
-// Mult.d F4, F2, F20
-// Ld F6, 0(R2)
-// Add.d F6, F4, F6
-// Sd F6, 0(R2)
-// Addi R1, R1, -4
-// Addi R2, R2, -4
-// Bne R1, R0, -8
-// Add.d F20, F2, F2
+
+
+// Addi R3, R3, 12
+// Mult.d R6, R5, R1
+// Beq R3, R6, -3
+
+
+// Addi R3, R3, 10
+// Addi R4, R4, -10
+// Beq R3, R4, -3
